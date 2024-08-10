4d62759cc7c2050cab01e7ad88e2fa45b1227b32
vjaykrsna <vijay74684@gmail.com>
2024-08-11 00:47:56 +0530

    added basic function

--- main.cpp
+++ main.cpp
@@ -8,10 +8,9 @@
 using namespace std;
 namespace fs = filesystem;
 
-string path = "/";
+fs::path path = "/";
 
-void home()
-{
+void home(){
     struct winsize ws;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
     int w = ws.ws_col;     // Get terminal width
@@ -19,30 +18,27 @@ void home()
 
     string text = "FILE MANAGER BY VIJAY KRISHNA 😎.";
     system("clear");
-    for (int i = 0; i < h / 2; i++)
-    {
+    for (int i = 0; i < h / 2; i++){
         cout << string(w - 1, '`') << endl;
     }
 
     cout << string((w - text.size()) / 2, '`') + " " + text + string((w - text.size()) / 2, '`') << endl;
 
-    for (int i = 0; i < h / 2; i++)
-    {
+    for (int i = 0; i < h / 2; i++){
         cout << string(w - 1, '`') << endl;
     }
 
     getchar();
     system(("clear"));
+    list(path);
 }
 
-string fSize(uintmax_t size)
-{
+string fSize(size_t size){
     const char *suffixes[] = {"bytes", "KB", "MB", "GB", "TB"};
     int i = 0;
     double doubleSize = static_cast<double>(size);
 
-    while (doubleSize >= 1024 && i < 4)
-    {
+    while (doubleSize >= 1024 && i < 4){
         doubleSize /= 1024;
         i++;
     }
@@ -50,21 +46,17 @@ string fSize(uintmax_t size)
     return to_string(doubleSize).substr(0, to_string(doubleSize).find(".") + 3) + " " + suffixes[i];
 }
 
-void list(const string &path)
-{
+void list(const fs::path& path){
     cout << left << setw(50) << "Filename" << "Size" << endl;
     cout << string(60, '-') << endl; // Divider
 
-    for (const auto &entry : fs::directory_iterator(path))
-    {
+    for (const auto &entry : fs::directory_iterator(path)){
         cout << left << setw(50) << entry.path().filename().string();
 
-        if (fs::is_directory(entry.path()))
-        {
+        if (fs::is_directory(entry.path())){
             cout << "dir";
         }
-        else
-        {
+        else{
             cout << fSize(fs::file_size(entry.path()));
         }
 
@@ -72,37 +64,62 @@ void list(const string &path)
     }
 }
 
-void create(const string &name)
-{
+void create(const string& name){
     ofstream outFile(name);
-    if (!outFile)
-    {
+    if (!outFile){
         cout << "Failed to create file." << endl;
     }
 }
 
-void del(const string &path)
-{
-    if (!fs::remove(path))
-    {
+void del(const fs::path& path){
+    if (!fs::remove(path)){
         cout << "File not found" << endl;
     }
 }
 
-void renameFile(const string &oldN, const string &newN)
-{
-    if (fs::exists(oldN))
-    {
+void rename(const fs::path& oldN, const fs::path& newN){
+    if (fs::exists(oldN)){
         fs::rename(oldN, newN);
     }
-    else
-    {
+    else{
         cout << "File doesn't exist" << endl;
     }
 }
 
+void copy(const fs::path& source, const fs::path& destination){
+    if(fs::exists(destination)){
+        cout<<"File already exist in destination, do you want to overwrite on cancel?"<<endl;
+        cout<<"'c' for canel, 'o' for overwrite"<<endl;
+        char ch;
+        cin>>ch;
+        if(ch=='c') return;
+    }
+    else {
+        cout<<"File doesn't exists"<<endl;
+        return;
+    }
+
+    fs::copy(source, destination, fs::copy_options::overwrite_existing);
+}
+
+void move(const fs::path& source, const fs::path& destination){
+    if(fs::exists(destination)){
+        cout<<"File already exist in destination, do you want to overwrite on cancel?"<<endl;
+        cout<<"'c' for canel, 'o' for overwrite"<<endl;
+        char ch;
+        cin>>ch;
+        if(ch=='c') return;
+    }
+    else {
+        cout<<"File doesn't exists"<<endl;
+        return;
+    }
+    
+    fs::rename(source, destination);
+}
+
+void back(const fs::path&  )
 int main(){
     home();
-    list(path);
     return 0;
 }
